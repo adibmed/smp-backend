9f300f728c7acaf242c77b28200ac2abb28e44d5
adibemohamed <mr.eladib.mohamed@gmail.com>
2022-02-23 23:12:37 +0100

    chore: add Product repository

---
+++ app/Repositories/Product/ProductRepository.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Repositories\Product;
+
+use App\Models\Product;
+use Illuminate\Support\Collection;
+
+class ProductRepository
+{
+
+
+    /**
+     * Return a collection of Products
+     *
+     * @return Collection
+     */
+    public function getAllProducts(): Collection
+    {
+        return Product::all();
+    }
+
+    /**
+     * Return a collection of Products with approved status
+     *
+     * @return Collection
+     */
+    public function getAllApprovedProducts(): Collection
+    {
+        return Product::all();
+    }
+
+    /**
+     * Create a new Product
+     *
+     * @return Product
+     */
+    public function createProduct(array $productDetails): Product
+    {
+        return Product::create($productDetails);
+    }
+
+    /**
+     * Update a Product details
+     *
+     * @return Product
+     */
+    public function updateProduct($productId, array $productDetails): Product
+    {
+        return Product::whereId($productId)->udpate($productDetails);
+    }
+
+    /**
+     * Remove a Product
+     *
+     */
+    public function removeProduct($productId)
+    {
+        Product::destroy($productId);
+    }
+
+}
