2e44696b9f297aa7a5f56b08c0da084bd9ae4036
adibemohamed <mr.eladib.mohamed@gmail.com>
2022-02-23 19:07:33 +0100

    chore: add eloquent repositoty interface

---
+++ app/Repositories/EloquentRepositoryInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Repositories;
+
+interface EloquentRepositoryInterface
+{
+    /**
+     * @param array $attributes
+     * @return Model
+     */
+    public function create(array $attributes): Model;
+
+    /**
+     * @param $id
+     * @return Model
+     */
+    public function find($id): ?Model;
+}
