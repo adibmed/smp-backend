26c3b53dc1d12c9b38a34e924f9a8b28fd014e27
adibemohamed <mr.eladib.mohamed@gmail.com>
2022-02-23 19:14:55 +0100

    chore: add user repository

---
+++ app/Repositories/Eloquent/UserRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Models\User;
+use Illuminate\Support\Collection;
+
+class UserRepository
+{
+    /**
+     * UserRepository constructor.
+     *
+     * @param User $model
+     */
+    public function __construct(User $model)
+    {
+        parent::__construct($model);
+    }
+
+    /**
+     * @return Collection
+     */
+    public function all(): Collection
+    {
+        return $this->model->all();
+    }
+}
